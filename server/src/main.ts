c8f8cc9e4dadf8aabb6d2fddea3d62d9781d01b4
webgzh907247189 <18255106634@163.com>
2019-03-18 21:38:45 +0800

    update nest init

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,11 +5,9 @@ const path = require('path')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.useStaticAssets(path.resolve(__dirname,'../public'))
   app.setBaseViewsDir(path.resolve(__dirname,'../views'))
-
-  console.log(path.resolve(__dirname,'../public'),'1111111111')
-  console.log(path.join(__dirname,'../views'),'44444444444')
   app.engine('html',swig.renderFile)
   app.setViewEngine('html')
 
